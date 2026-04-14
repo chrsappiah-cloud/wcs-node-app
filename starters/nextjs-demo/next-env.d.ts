78b5a69b2cabdb171a0ce306362a445efc4d27eb
Christopher Appiah-Thompson <chrsappiah@gmail.com>
2026-04-14 16:00:44 +1000

    docs: add proprietary license and tighten source headers

--- starters/nextjs-demo/next-env.d.ts
+++ starters/nextjs-demo/next-env.d.ts
@@ -1,4 +1,3 @@
-// Copyright (c) 2026 World Class Scholars, led by Dr Christopher Appiah-Thompson. All rights reserved.
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
--- starters/nextjs-demo/next.config.mjs
+++ starters/nextjs-demo/next.config.mjs
@@ -1,4 +1,3 @@
-// Copyright (c) 2026 World Class Scholars, led by Dr Christopher Appiah-Thompson. All rights reserved.
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
